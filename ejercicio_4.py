26981cecfd5b4094905eb1f1aee48818d659e2ac
sergio garcia <garciasosass583@gmail.com>
2025-08-02 11:45:37 -0500

    archivo listo

---
+++ ejercicio_4.py
@@ -0,0 +1,8 @@
+#tabla de multiplicar
+num = int(input("Ingrese un numero: "))
+'''
+estamos utulizando la funcion range que nos permite crear una secuencia de numeros
+el primero es donde empieza y el segundo es donde termina y asi vamos a imprimir la tabla de multiplciar dependiendo del numero infgresado
+'''
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
